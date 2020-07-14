748c3e46e94a1df1099a90f1cf6da5c9056cfab6
diaz227 <diaz.227@osu.edu>
2020-07-14 13:54:55 -0400

    -Testing the staging onto local changed the tesxt again.

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>First React Project Text!</Text>
+      <Text>First React Project Text yay!</Text>
       <StatusBar style="auto" />
     </View>
   );
